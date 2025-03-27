80cde0a080d9c530e81f35b4c70fbc305016a3c5
JuanJRos <jjrosado91@gmail.com>
2025-03-27 16:45:29 +0100

    Add files via upload

---
+++ Sintaxis-2/Sintaxis-2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Sintaxis_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine($"El número {numero1} es mayor a {numero2}.");
+            }
+            else if (numero2 > numero1)
+            {
+                Console.WriteLine($"El número {numero2} es mayor a {numero1}");
+            }
+            else
+            {
+                Console.WriteLine("Los dos número son iguales");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ Sintaxis-3/Sintaxis-3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sintaxis_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce día de la semana: ");
+            string dia = Console.ReadLine().ToLower();
+
+            switch (dia)
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("Introduce un día de la semana");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

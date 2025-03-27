51cb440e3cfda2a409e943af4ade9106974bb392
JuanJRos <jjrosado91@gmail.com>
2025-03-27 16:44:01 +0100

    Add files via upload

---
+++ Sintaxis-1/Sintaxis-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sintaxis_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduce tu edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce tu ciudad: ");
+            string ciudad = Console.ReadLine();
+            Console.WriteLine($"Te llamas {nombre} tienes {edad} años. Bienvenido a {ciudad}");
+            Console.ReadLine();
+        }
+    }
+}

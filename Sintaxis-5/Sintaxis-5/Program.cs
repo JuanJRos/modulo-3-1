2495f0b6c62bddd839633681edf99083f215de2a
JuanJRos <jjrosado91@gmail.com>
2025-03-27 16:46:19 +0100

    Add files via upload

---
+++ Sintaxis-5/Sintaxis-5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sintaxis_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Ingrese un número: ");
+            int numero = Convert.ToInt32 (Console.ReadLine());
+
+            List<int> numerosRandoms = new List<int>();
+
+            for (int i = 0; i < numero; i++)
+            {
+                numerosRandoms.Add(random.Next(0,200));
+            }
+
+            Console.WriteLine("Numeros generados");
+
+            foreach (int num in numerosRandoms)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();    
+            
+        }
+    }
+}
---
+++ Sintaxis-6/Sintaxis-6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sintaxis_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un caracter");
+            string caracter = Console.ReadLine();
+
+            Console.WriteLine("----------");
+
+            for (int i = 0; i < numero; i++)
+            {
+                int fila = 0;
+                string filaCadena = "";
+
+                while(fila <= i)
+                {
+                    if(i == numero-1 || fila == 0 || fila == i)
+                    {
+                        filaCadena += caracter;
+                    }
+                    else
+                    {
+                        filaCadena += " ";
+                    }
+                    fila++;
+                }
+                Console.WriteLine(filaCadena);
+            }
+
+        }
+    }
+}
